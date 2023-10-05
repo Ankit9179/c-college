7d6047d4a1bbc2933050822606a78a4fe7a0b020
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-10-05 23:13:48 +0530

    change some code ,function code

--- c-language/divisibleBy7/fNumDivisibleBy7.c
+++ c-language/divisibleBy7/fNumDivisibleBy7.c
@@ -4,14 +4,14 @@ int main()
     int x;
     printf("give me a number : ");
     scanf("%d", &x);
-    // check condition for devisible by 7 or not
+    // check condition for divisible by 7 or not
     if (x % 7 == 0)
     {
-        printf(" the num %d is devisible by 7 ", x);
+        printf(" the num %d is divisible by 7 ", x);
     }
     else
     {
-        printf(" the num %d is not devisible by 7 ", x);
+        printf(" the num %d is not divisible by 7 ", x);
     }
     return 0;
 }
---
+++ c-language/functionsinc/greet.c
@@ -0,0 +1,13 @@
+// definition of function -1. a function is a black of code which is runs only when it is colled.
+#include <stdio.h>
+// define greet func
+void greet()
+{
+    printf("hello , good morning ak");
+}
+// here int and void is return type
+int main()
+{
+    // function colling
+    greet();
+}
