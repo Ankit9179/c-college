0bc692552e5dd351e90ff7cf57963cf773c1d5f8
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-12-05 10:20:15 +0530

    add folder for string

---
+++ c-language/strings/first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/// #1
+void main()
+{
+    printf("Hello,world! \nwelcome to my first program in c.");
+}
+
+/// #2
+// int main()
+// {
+//     printf("Hello,world! \nwelcome to c program.");
+//     return 0;
+// }
